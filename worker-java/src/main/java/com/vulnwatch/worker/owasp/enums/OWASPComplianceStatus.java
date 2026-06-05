023efa5183e4d1457f72eed7df13c103f7f33622
Mitchel Ntuen <mitchelntuen@gmail.com>
2026-06-05 18:51:11 +0100

    feat: add owaspcompliancestatus

--- worker-java/src/main/java/com/vulnwatch/worker/owasp/enums/OWASPComplianceStatus.java
+++ worker-java/src/main/java/com/vulnwatch/worker/owasp/enums/OWASPComplianceStatus.java
@@ -1,4 +1,13 @@
 package com.vulnwatch.worker.owasp.enums;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum OWASPComplianceStatus {
+    COMPLIANT,        // no findings in this category
+
+    PARTIAL,          // only LOW severity findings
+
+    NON_COMPLIANT     // any MEDIUM / HIGH / CRITICAL finding
+
 }
