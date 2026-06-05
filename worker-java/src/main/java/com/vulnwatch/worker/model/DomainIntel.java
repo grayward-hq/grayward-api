eee8804dc87134c1da997332bc9513c9f78c2941
mouse1999 <kufreedward26@gmail.com>
2026-06-05 17:59:15 +0100

    feat: introduced owasp result score and tier

--- worker-java/src/main/java/com/vulnwatch/worker/model/DomainIntel.java
+++ worker-java/src/main/java/com/vulnwatch/worker/model/DomainIntel.java
@@ -1,6 +1,7 @@
 package com.vulnwatch.worker.model;
 
 import com.vulnwatch.worker.enums.AiAvailability;
+import com.vulnwatch.worker.owasp.model.OWASPEvaluationResult;
 
 import java.time.Instant;
 
@@ -10,16 +11,21 @@ public record DomainIntel(
         String domainName,
         String requestedBy,
         int securityScore,
+        int owaspScore,          // ← NEW
+        String owaspTier,        // ← NEW  e.g. "Good"
         AiAvailability aiAvailability,
         Instant completedAt
 ) {
-    public static DomainIntel of(ScanJob job, int securityScore, AiAvailability aiAvailability) {
+    public static DomainIntel of(
+            ScanJob job,
+            int securityScore,
+            OWASPEvaluationResult owaspResult,
+            AiAvailability aiAvailability) {
         return new DomainIntel(
-                job.scanId(),
-                job.domainId(),
-                job.domainName(),
-                job.requestedBy(),
+                job.scanId(), job.domainId(), job.domainName(), job.requestedBy(),
                 securityScore,
+                owaspResult.overallScore(),
+                owaspResult.tier().getLabel(),
                 aiAvailability,
                 Instant.now()
         );
