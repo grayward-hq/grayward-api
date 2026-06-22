e43d209913561793d9aeec46bf4895498de9bb9f
Dimojiaku Obinna Anthony <dhymyantt1@gmail.com>
2026-06-22 16:54:01 +0100

    Move HTTP status mapping out of Domain to preserve layer boundaries.

--- api-dotnet/src/Domain/Common/Exception.cs
+++ api-dotnet/src/Domain/Common/Exception.cs
@@ -1,21 +1,5 @@
 using Domain.Enums;
-using Microsoft.AspNetCore.Http;
-
 namespace Domain.Common;
-
-public static class ErrorCodeExtensions
-{
-    public static int ToStatusCode(this ErrorCode code) => code switch
-    {
-        ErrorCode.NotFound    => StatusCodes.Status404NotFound,
-        ErrorCode.Conflict    => StatusCodes.Status409Conflict,
-        ErrorCode.Validation  => StatusCodes.Status400BadRequest,
-        ErrorCode.Unauthorized=> StatusCodes.Status401Unauthorized,
-        ErrorCode.Forbidden   => StatusCodes.Status403Forbidden,
-        ErrorCode.RateLimited => StatusCodes.Status429TooManyRequests,
-        _                     => StatusCodes.Status500InternalServerError,
-    };
-}
 
 public class AppException : Exception
 {
--- api-dotnet/src/Web/Middleware/ExceptionHandlingMiddleware.cs
+++ api-dotnet/src/Web/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Domain.Common;
+using Web.Extensions;
 
 namespace Web.Middleware;
 
