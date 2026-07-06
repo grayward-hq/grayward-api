98276eab573056f926d169789aad7a7a673f033f
mouse1999 <kufreedward26@gmail.com>
2026-07-06 14:57:39 +0100

    feat: add an exception for the repo scan

--- worker-java/src/main/java/com/vulnwatch/worker/exception/RepositoryScanException.java
+++ worker-java/src/main/java/com/vulnwatch/worker/exception/RepositoryScanException.java
@@ -1,4 +1,12 @@
 package com.vulnwatch.worker.exception;
 
-public class RepositoryScanException {
+/** Signals a retryable failure during repository scan execution (clone, auth, Trivy invocation). */
+public class RepositoryScanException extends RuntimeException {
+    public RepositoryScanException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RepositoryScanException(String message) {
+        super(message);
+    }
 }
